5168dbac3f2d1b263e9f94d0f0c9b48d3050a81b
Samikshya <write2samikshya@gmail.com>
2021-05-18 07:36:17 +0530

    Add project files.

---
+++ Samikshya_Ado.Net_Part1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Samikshya_Ado.Net_Part1.Global" Language="C#" %>
---
+++ Samikshya_Ado.Net_Part1/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Samikshya_Ado.Net_Part1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection("data source =.; database=Sample;Integrated Security=SSPI");
+            SqlCommand cmd = new SqlCommand("select * from tblemployee", con);
+
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+
+
+
+        }
+    }
+}
